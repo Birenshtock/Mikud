50e598dae75be2b20f6230e682553fefd4ba31f5
adi birenshtock <adibirenshtock1@gmai.com>
2023-02-06 17:36:37 +0200

    adi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -207,8 +207,7 @@
               <p>
                 {{ lorem }}
               </p>
-              <!-- <button class="learning-btn">למד עוד</button>
-              <RouterLink to="/contactUs"><button class="learning-btn">הרשם</button></RouterLink> -->
+              <button class="learning-btn">למד עוד</button>
             </div>
           </div>
           <div class="learning-item">
@@ -224,8 +223,7 @@
               <p>
                 {{ lorem }}
               </p>
-              <!-- <button class="learning-btn">למד עוד</button>
-              <RouterLink to="/contactUs"><button class="learning-btn">הרשם</button></RouterLink> -->
+              <button class="learning-btn">למד עוד</button>
             </div>
           </div>
           <div class="learning-item">
@@ -240,8 +238,7 @@
               <p>
                 {{ lorem }}
               </p>
-              <!-- <button class="learning-btn">למד עוד</button>
-              <RouterLink to="/contactUs"><button class="learning-btn">הרשם</button></RouterLink> -->
+              <button class="learning-btn">למד עוד</button>
             </div>
           </div>
         </div>
